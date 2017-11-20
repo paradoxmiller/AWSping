2308da43c73d96a00159036503fdfc65af40cedf
paradoxmiller <33523467+paradoxmiller@users.noreply.github.com>
2017-11-20 10:20:10 -0800

    Create awsping.sh
    This is used to query EC2 instances, put them in a text file, and then ping the list from said text file.  NOTE: ICMP traffic will need to be permitted for the given security groups for the EC2 instances in order for them to respond positively to ping.

---
+++ awsping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script will query the Public DNS Names of AWS instances
+# It will save them to a list 'ec2list.txt' and then ping them
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text >ec2list.txt
+
+cat ~/Desktop/AWSTUFF/ec2list.txt | while read output
+do
+	ping -c 1 "$output" > /dev/null
+	if [ $? -eq 0 ]; then
+		setterm -term linux -back black -fore green
+		echo "$output responds to ping"
+	else
+		setterm -term linux -back white -fore red		
+		echo "$output is NOT responding"
+	fi
+tput sgr0
+done
